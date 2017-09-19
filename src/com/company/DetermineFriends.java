d21463d2286c8add3d5fd3fb92afbad8b0f2472b
Marvin Do <marvin.do@everykey.com>
2017-09-19 16:08:03 -0400

    finished prototype of traversing through dictionary

--- src/com/company/DetermineFriends.java
+++ src/com/company/DetermineFriends.java
@@ -15,7 +15,7 @@ public class DetermineFriends {
     public static boolean isFriends(String first, String second) {
         // Check if it's the same string
         if (first.equals(second)) {
-            System.out.println("It's the same word");
+            //System.out.println("It's the same word");
             return false;
         }
         // Same length, checks to see if it requires only one replacement
@@ -32,7 +32,7 @@ public class DetermineFriends {
             return isOneCharReplacement(second, first);
         }
 
-        System.out.println(first + " is not friends with" + " " + second);
+        //System.out.println(first + " is not friends with" + " " + second);
         return false;
     }
 
@@ -44,18 +44,17 @@ public class DetermineFriends {
      */
     private static boolean isOneCharReplacement(String first, String second) {
         boolean foundDifference = false;
-        for (int i = 0; i < first.length(); i ++) {
-            if (first.charAt(i) != first.charAt(i)) {
+        for (int i = 0; i < first.length(); i++) {
+            if (first.charAt(i) != second.charAt(i)) {
 
                 // Found another different char after previously finding one
                 if (foundDifference) {
                     return false;
                 }
-
                 foundDifference = true;
             }
         }
-        System.out.println(first + " is friends with " + second);
+        //System.out.println(first + " is friends with " + second);
         return true;
     }
 
@@ -66,7 +65,6 @@ public class DetermineFriends {
      * @return
      */
     private static boolean isOneCharAddition(String first, String second) {
-        boolean foundDifference = false;
         int firstIndex = 0;
         int secondIndex = 0;
 
@@ -74,26 +72,23 @@ public class DetermineFriends {
             // Found difference
             if (first.charAt(firstIndex) != second.charAt(secondIndex)) {
                 // If already found difference before
-                if (foundDifference) {
+                if (firstIndex != secondIndex) {
                     return false;
                 }
                 secondIndex++;
-            }
-
-            else {
+            } else {
                 firstIndex++;
                 secondIndex++;
             }
         }
-        System.out.println(first + " is friends with " + second);
+        //System.out.println(first + " is friends with " + second);
         return true;
     }
 
     //To Test
      public static void main(String[] args) {
-     isFriends("HE", "HER");
+     System.out.println(isFriends("LISTY", "LITCHI"));
      System.out.println("\n");
-     isFriends("HE", "HERE");
      }
 
 }
--- src/com/company/SocialNetwork.java
+++ src/com/company/SocialNetwork.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 /**
+ * Assumptions: There are no duplicates in the dictionary
+ *              All of the words in the dictionary are capitalized
  * Created by marvindo on 9/15/17.
  */
 public class SocialNetwork {
@@ -14,22 +16,63 @@ public class SocialNetwork {
         dictionary = new ReadDictionary(filePath).getDictionary();
     }
 
-    public int findNetworkSize(String source, ArrayList<String> dictionary_to_traverse) {
-        //Removes input string itself from the dictionary to avoid loops
-        dictionary_to_traverse.remove(dictionary_to_traverse.indexOf(source));
-        List<String> = 
 
+    public int findNetworkSize(String source) {
+        //Removes input string itself from the dictionary to avoid loops
+        ArrayList<String> dictionary_to_traverse = dictionary;
+        dictionary_to_traverse.remove(dictionary_to_traverse.indexOf(source));
+
+        // We include the additional 1 to the size to account for source.
+        return 1 + recursiveFriendNetwork(source, dictionary_to_traverse);
     }
 
+
+    /*
+    Two strings cannot be direct friends if it is two or more direct length away from each other.
+     */
+    private int recursiveFriendNetwork(String source, ArrayList<String> dictionary_to_traverse) {
+        int index = 0;
+        LinkedList<String> isFriends = new LinkedList<String>();
+        // Update index so it starts from length - 1 of source
+        while (dictionary_to_traverse.get(index).length() < source.length() - 1) {
+            index++;
+        }
+        while (dictionary_to_traverse.get(index).length() < source.length() + 2 &&
+                index < dictionary_to_traverse.size()) {
+
+            //If they are friends
+            if( DetermineFriends.isFriends(source, dictionary_to_traverse.get(index)) ) {
+                // add it to the isFriends list
+                isFriends.add(dictionary_to_traverse.remove(index));
+
+            }
+            index++;
+        }
+
+        /*********************
+        Tail recursive
+         *********************/
+        if (isFriends.isEmpty()) {
+            return 0;
+        }
+        else {
+            //Iterate through the isFriends to find their friends as well.
+            int friend_network = isFriends.size();
+            for (int i = 0; i < isFriends.size(); i++) {
+                int friendPointer = recursiveFriendNetwork(isFriends.get(i), dictionary_to_traverse);
+
+                friend_network += friendPointer;
+            }
+            return friend_network;
+        }
+    }
 
 
 
     // For Testing
     public static void main(String[] args) {
-        SocialNetwork network = new SocialNetwork("very_small_test_dictionary.txt");
-        for (int i =0; i < network.dictionary.size(); i++) {
-            System.out.println(network.dictionary.get(i));
-        }
+        SocialNetwork network = new SocialNetwork("example.txt");
+        System.out.println(network.findNetworkSize("HI"));
     }
 
 }
